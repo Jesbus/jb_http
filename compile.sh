787aad4e4bfc938fc39e4a71966e15d8b5a5912d
Jesbus <jessebusman@gmail.com>
2014-03-02 11:56:34 +0100

    Improved compile script a bit

--- compile.sh
+++ compile.sh
@@ -3,21 +3,9 @@
 clear
 echo "################################################################"
 echo "################################################################"
-echo "################################################################"
-echo "################################################################"
-echo "################################################################"
-echo "################################################################"
-echo "################################################################"
-echo "################################################################"
-echo "################################################################"
-echo "################################################################"
 
 cd binaries
 
-if [ $1 == "main" ] || [ $1 == "all" ]; then
-	echo "Compiling main..."
-	g++ -c ../main.cpp -Wno-write-strings -std=c++11 -lboost_regex 2>&1 | grep error
-fi
 if [ $1 == "definitions" ] || [ $1 == "all" ]; then
 	echo "Compiling defs..."
 	g++ -c ../definitions.cpp -Wno-write-strings -std=c++11 -lboost_regex 2>&1 | grep error
@@ -33,6 +21,10 @@ fi
 if [ $1 == "compiler" ] || [ $1 == "all" ]; then
 	echo "Compiling compiler..."
 	g++ -c ../compiler.cpp -Wno-write-strings -std=c++11 -lboost_regex 2>&1 | grep error
+fi
+if [ $1 == "main" ] || [ $1 == "all" ]; then
+	echo "Compiling main..."
+	g++ -c ../main.cpp -Wno-write-strings -std=c++11 -lboost_regex 2>&1 | grep error
 fi
 echo "Linking..."
 g++ main.o compiler.o parser.o interpreter.o definitions.o -Wno-write-strings -std=c++11 -lboost_regex -o jb_http
